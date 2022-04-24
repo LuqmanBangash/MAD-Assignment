aee571878abf109221967a893dba888196e32389
Luqman Khan <kuqmankhan.bangash@gmail.com>
2022-04-24 22:14:33 +0500

    Assignment

---
+++ app/src/main/java/com/example/assignment_mad/Welcome.java
@@ -0,0 +1,11 @@
+package com.example.assignment_mad;
+
+import android.os.Bundle;
+
+public class Welcome {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
